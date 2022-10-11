ca6e44eb5065a0798ccadf6488ddb8d18a0b026b
Jacarder <jonandrewcarder@gmail.com>
2022-10-11 19:59:52 -0400

    Refactor service object to return unsubscribe instead of within promise

--- src/services/HackerNews.service.ts
+++ src/services/HackerNews.service.ts
@@ -1,11 +1,11 @@
 import { db } from "../utils/firebase"
 import { DataSnapshot, onValue, ref } from "firebase/database"
 export default new class HackerNewsService {
-	getTopNews = () => ({
+	getTopNews = {
 		value: async <T>() => (await db.ref('v0/topstories').get()).val() as T,
-		subscribe: async (callback: (snapshot: DataSnapshot) => void) => onValue(ref(db, 'v0/topstories'), callback)
-	})
-	getItemById = (id: number) => ({
-		value: async <T>() => (await db.ref(`v0/item/${id}`).get()).val() as T,
-	})
+		subscribe: (callback: (snapshot: DataSnapshot) => void) => onValue(ref(db, 'v0/topstories'), callback)
+	}
+	getItemById = {
+		value: async <T>(id: number) => (await db.ref(`v0/item/${id}`).get()).val() as T,
+	}
 }()
